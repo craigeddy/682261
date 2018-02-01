dfbecbcea86ddd1683baf669f2a3597510050f47
Craig Eddy <eddycr@state.gov>
2018-02-01 07:30:23 -0500

    Initial commit

---
+++ GoogleAuthenticator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GoogleAuthenticator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSetupCode = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSetupCode
+            // 
+            this.txtSetupCode.Location = new System.Drawing.Point(296, 33);
+            this.txtSetupCode.Multiline = true;
+            this.txtSetupCode.Name = "txtSetupCode";
+            this.txtSetupCode.Size = new System.Drawing.Size(281, 106);
+            this.txtSetupCode.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 223);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 353);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(165, 22);
+            this.txtCode.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Authenticator Code:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Authenticator Setup";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtSetupCode);
+            this.Name = "Form1";
+            this.Text = "Google Authenticator Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSetupCode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GoogleAuthenticator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Authenticator;
+
+namespace GoogleAuthenticator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TwoFactorAuthenticator tfA = new TwoFactorAuthenticator();
+            var setupCode = tfA.GenerateSetupCode("FACTSInfo", "Craig Eddy", Guid.NewGuid().ToString("N"), 
+                pictureBox1.Width, pictureBox1.Height);
+            WebClient wc = new WebClient();
+            MemoryStream ms = new MemoryStream(wc.DownloadData(setupCode.QrCodeSetupImageUrl));
+            this.pictureBox1.Image = Image.FromStream(ms);
+
+            this.txtSetupCode.Text = "Account: " + setupCode.Account + System.Environment.NewLine +
+                                     "Secret Key: " + setupCode.AccountSecretKey + System.Environment.NewLine +
+                                     "Encoded Key: " + setupCode.ManualEntryKey;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TwoFactorAuthenticator tfA = new TwoFactorAuthenticator();
+            var result = tfA.ValidateTwoFactorPIN("f68f1fe894d548a1bbc66165c46e61eb", this.txtCode.Text);
+
+            MessageBox.Show(result ? "Validated!" : "Incorrect", "Result");
+        }
+    }
+}
